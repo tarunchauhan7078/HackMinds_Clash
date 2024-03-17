4dd47a681ae53fbbec311fb5215d16620f94bd82
sushmitadassharma <161571442+sushmitadassharma@users.noreply.github.com>
2024-03-17 18:00:19 +0530

    css file name change
    Register file name css change

--- src/components/Registration/Register.jsx
+++ src/components/Registration/Register.jsx
@@ -11,7 +11,7 @@ import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import './Style.css';
+import './Register.css';
 // import { CreateContext } from "../../App";
 import { useContext } from "react";
 
